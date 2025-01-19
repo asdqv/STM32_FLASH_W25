db6709df9db6fde69b1cb452e48aace12dc943c1
ASDQV <gorodnov.artyom@yandex.ru>
2025-01-19 21:46:30 +0300

    full version reading ID code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,7 +41,7 @@
 #define w25_READ			0x03
 #define w25_GET_JDEC_ID		0x9f
 //макросы для управления ножкой выбора NSS
-#define cs_set()	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET)
+#define cs_set()	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET) //chip select, если ножка в низом уровне, отклик ведомого устройства разрешен, если в высоком, то запрещен
 #define cs_reset()	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET)
 
 /* USER CODE END PM */
@@ -108,7 +108,7 @@ void w25_Read_Data(uint32_t addr, uint8_t *dat, uint32_t sz){
 	cs_reset();
 }
 uint32_t w25_Read_ID(void){
-	uint8_t dt[4];
+	uint8_t dt[3];
 	tx_buf[0] = w25_GET_JDEC_ID;
 	cs_set();
 	SPI1_Send(tx_buf, 1);
@@ -124,6 +124,76 @@ void w25_Init(void){
 	HAL_UART_Transmit(&huart1,(uint8_t*)"\r\n",2,0x1000);
 	 sprintf(str,"ID:0x%X\r\n", ID);
 	 HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	 ID &= 0x0000ffff; //маска отсечения manuf ID
+	 switch(ID)
+	  {
+	    case 0x401A:
+	      w25_info.BlockCount=1024;
+	      sprintf(str,"w25qxx Chip: w25q512\r\n");
+	      break;
+	    case 0x4019:
+	      w25_info.BlockCount=512;
+	      sprintf(str,"w25qxx Chip: w25q256\r\n");
+	      break;
+	    case 0x4018:
+	      w25_info.BlockCount=256;
+	      sprintf(str,"w25qxx Chip: w25q128\r\n");
+	      break;
+	    case 0x4017:
+	      w25_info.BlockCount=128;
+	      sprintf(str,"w25qxx Chip: w25q64\r\n");
+	      break;
+	    case 0x4016:
+	      w25_info.BlockCount=64;
+	      sprintf(str,"w25qxx Chip: w25q32\r\n");
+	      break;
+	    case 0x4015:
+	      w25_info.BlockCount=32;
+	      sprintf(str,"w25qxx Chip: w25q16\r\n");
+	      break;
+	    case 0x4014:
+	      w25_info.BlockCount=16;
+	      sprintf(str,"w25qxx Chip: w25q80\r\n");
+	      break;
+	    case 0x4013:
+	      w25_info.BlockCount=8;
+	      sprintf(str,"w25qxx Chip: w25q40\r\n");
+	      break;
+	    case 0x4012:
+	      w25_info.BlockCount=4;
+	      sprintf(str,"w25qxx Chip: w25q20\r\n");
+	      break;
+	    case 0x4011:
+	      w25_info.BlockCount=2;
+	      sprintf(str,"w25qxx Chip: w25q10\r\n");
+	      break;
+	    default:
+	      sprintf(str,"w25qxx Unknown ID\r\n");
+	      HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	      return;
+	  }
+	  HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	  HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	   w25_info.PageSize=256;
+	   w25_info.SectorSize=0x1000;
+	   w25_info.SectorCount=w25_info.BlockCount*16;
+	   w25_info.PageCount=(w25_info.SectorCount*w25_info.SectorSize)/w25_info.PageSize;
+	   w25_info.BlockSize=w25_info.SectorSize*16;
+	   w25_info.NumKB=(w25_info.SectorCount*w25_info.SectorSize)/1024;
+	   sprintf(str,"Page Size: %d Bytes\r\n",(unsigned int)w25_info.PageSize);
+	   HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	   sprintf(str,"Page Count: %u\r\n",(unsigned int)w25_info.PageCount);
+	   HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	   sprintf(str,"Sector Size: %u Bytes\r\n",(unsigned int)w25_info.SectorSize);
+	   HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	   sprintf(str,"Sector Count: %u\r\n",(unsigned int)w25_info.SectorCount);
+	   HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	   sprintf(str,"Block Size: %u Bytes\r\n",(unsigned int)w25_info.BlockSize);
+	   HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	   sprintf(str,"Block Count: %u\r\n",(unsigned int)w25_info.BlockCount);
+	   HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
+	   sprintf(str,"Capacity: %u KB\r\n",(unsigned int)w25_info.NumKB);
+	   HAL_UART_Transmit(&huart1,(uint8_t*)str,strlen(str),0x1000);
 
 }
 /* USER CODE END 0 */
@@ -158,7 +228,6 @@ int main(void)
   MX_SPI1_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  //HAL_UART_Transmit(&huart1, (uint8_t*)"Hello World\n", 12, 1000);
   w25_Init();
   /* USER CODE END 2 */
 
